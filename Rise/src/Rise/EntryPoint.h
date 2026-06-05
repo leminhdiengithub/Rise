5f58053371a00f646249806ecc6d66585b52d0f2
unknown <leminhdien20043333@gmail.com>
2026-06-05 13:01:34 +0700

    Premake

--- Rise/src/Rise/EntryPoint.h
+++ Rise/src/Rise/EntryPoint.h
@@ -9,10 +9,14 @@ int main(int argc, char* argv)
     Rise::Log::Init();
     RS_CORE_WARN("Initialized Log!");
     int a = 5;
-    RS_CORE_INFO("Hi Var={0}", a);
+    RS_INFO("Hello Var={0}", a);
 
     auto app = Rise::CreateApplication();
     app->Run();
     delete app;
 }
 #endif 
+
+/*
+extern : khai báo một hàm được định nghĩa ở file khác.
+*/ 
---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Rise"
+    architecture "x64"
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Rise"
+    location "Rise"
+    kind "SharedLib"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs 
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"     
+        staticruntime "On"
+        systemversion "latest"
+        buildoptions { "/utf-8" }
+
+        defines
+        {
+            "RS_PLATFORM_WINDOWS",
+            "RS_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox") 
+        }
+
+    filter "configurations:Debug"
+        defines "RS_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RS_RELEASE"    
+        optimize "On"          
+
+    filter "configurations:Dist"
+        defines "RS_DIST"
+        optimize "On"         
+
+project "Sandbox"
+    location "Sandbox"      
+    kind "ConsoleApp"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")   
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")  
+
+    files
+    {
+        "%{prj.name}/src/**.h",    
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs                
+    {
+        "Rise/vendor/spdlog/include",
+        "Rise/src"
+    }
+
+    links
+    {
+        "Rise"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"     
+        staticruntime "On"
+        systemversion "latest"
+        buildoptions { "/utf-8" }
+
+        defines
+        {
+            "RS_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "RS_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "RS_RELEASE" 
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "RS_DIST"
+        optimize "On"
