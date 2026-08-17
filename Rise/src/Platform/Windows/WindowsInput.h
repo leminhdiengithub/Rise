869fb1a9a736b20e02a2948d1158a669e0a4c232
unknown <leminhdien20043333@gmail.com>
2026-08-17 15:12:08 +0700

    Input Polling system

---
+++ Rise/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Rise/Input.h"
+
+namespace Rise {
+
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual bool GetMouseXImpl() override;
+		virtual bool GetMouseYImpl() override;
+	};
+
+}
--- Rise/src/Rise/Application.cpp
+++ Rise/src/Rise/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Rise
 {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this , std::placeholders::_1)
@@ -63,6 +65,8 @@ namespace Rise
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
 
+            auto [x, y] = Input::GetMousePosition();
+            RS_CORE_TRACE("{0}, {1}", x, y);
             m_Window->OnUpdate();
         }
     }
